744fe6976870b2bc1d994f772c8dbb907f0f55b8
recepecem <61947822+recepecem@users.noreply.github.com>
2023-03-19 14:04:37 +0300

    Delete 7.sh

--- 7.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-count=$( ls -la $1 | wc -l )
-echo "$(($count-3))"	# wc command counts newlines in given piped command. 'ls -la' command lists current directory '.', parent directory '..' and total blocks of disc used by current directory.
